b029f1c8fa8fdb857790bf6f4ade47c1223bd385
Sascha Greuel <github@1-2.dev>
2022-09-18 22:51:43 +0200

    5.4.2

--- files/lib/system/condition/anniversary/AnniversaryDisabledCondition.class.php
+++ files/lib/system/condition/anniversary/AnniversaryDisabledCondition.class.php
@@ -1,5 +1,28 @@
 <?php
+
+/*
+ * Copyright by Udo Zaydowicz.
+ * Modified by SoftCreatR.dev.
+ *
+ * License: http://opensource.org/licenses/lgpl-license.php
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 namespace wcf\system\condition\anniversary;
+
+use InvalidArgumentException;
 use wcf\data\DatabaseObjectList;
 use wcf\data\user\UserList;
 use wcf\system\condition\AbstractCheckboxCondition;
@@ -7,31 +30,28 @@ use wcf\system\condition\IObjectListCondition;
 
 /**
  * Condition implementation to exclude disabled users.
- * 
- * @author		2017-2022 Zaydowicz.de
- * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @package		com.uz.wcf.box.anniversary
  */
-class AnniversaryDisabledCondition extends AbstractCheckboxCondition implements IObjectListCondition {
-		/**
-		 * @inheritDoc
-		 */
-		protected $fieldName = 'activationCode';
-		
-		/**
-		 * @inheritDoc
-		 */
-		protected $label = 'wcf.acp.box.anniversary.condition.disabled';
-		
-		/**
-		 * @inheritDoc
-		 */
-		public function addObjectListCondition(DatabaseObjectList $objectList, array $conditionData) {
-			if (!($objectList instanceof UserList)) {
-				throw new \InvalidArgumentException("Object list is no instance of '".UserList::class."', instance of '".get_class($objectList)."' given.");
-			}
-			
-			$objectList->getConditionBuilder()->add('user_table.activationCode = ?', [0]);
-		}
-	}
-	
+class AnniversaryDisabledCondition extends AbstractCheckboxCondition implements IObjectListCondition
+{
+    /**
+     * @inheritDoc
+     */
+    protected $fieldName = 'activationCode';
+
+    /**
+     * @inheritDoc
+     */
+    protected $label = 'wcf.acp.box.anniversary.condition.disabled';
+
+    /**
+     * @inheritDoc
+     */
+    public function addObjectListCondition(DatabaseObjectList $objectList, array $conditionData)
+    {
+        if (!($objectList instanceof UserList)) {
+            throw new InvalidArgumentException("Object list is no instance of '" . UserList::class . "', instance of '" . \get_class($objectList) . "' given.");
+        }
+
+        $objectList->getConditionBuilder()->add('user_table.activationCode = ?', [0]);
+    }
+}
--- files/lib/system/condition/anniversary/AnniversaryYearCondition.class.php
+++ files/lib/system/condition/anniversary/AnniversaryYearCondition.class.php
@@ -1,5 +1,28 @@
 <?php
+
+/*
+ * Copyright by Udo Zaydowicz.
+ * Modified by SoftCreatR.dev.
+ *
+ * License: http://opensource.org/licenses/lgpl-license.php
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 namespace wcf\system\condition\anniversary;
+
+use InvalidArgumentException;
 use wcf\data\DatabaseObjectList;
 use wcf\data\user\UserList;
 use wcf\system\condition\AbstractIntegerCondition;
@@ -7,78 +30,78 @@ use wcf\system\condition\IObjectListCondition;
 
 /**
  * Condition implementation to include years of membership.
- * 
- * @author		2017-2022 Zaydowicz.de
- * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
- * @package		com.uz.wcf.box.anniversary
  */
-class AnniversaryYearCondition extends AbstractIntegerCondition implements IObjectListCondition {
-	/**
-	 * @inheritDoc
-	 */
-	protected $fieldName = 'registrationDateYear';
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected $label = 'wcf.acp.box.anniversary.condition.year';
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected $description = 'wcf.acp.box.anniversary.condition.year.description';
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected $minValue = 0;
-	protected $greaterThan = 0;
-	
-	/**
-	 * @inheritDoc
-	 */
-	protected function getIdentifier() {
-		return 'year';
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function getData() {
-		$data = [];
-		
-		if ($this->lessThan !== null) {
-			$data['lessThan'] = $this->lessThan;
-		}
-		
-		if ($this->greaterThan !== null) {
-			$data['greaterThan'] = $this->greaterThan;
-		}
-		else {
-			$data['greaterThan'] = 0;
-		}
-		
-		if (!empty($data)) {
-			return $data;
-		}
-		
-		return null;
-	}
-	
-	/**
-	 * @inheritDoc
-	 */
-	public function addObjectListCondition(DatabaseObjectList $objectList, array $conditionData) {
-		if (!($objectList instanceof UserList)) {
-			throw new \InvalidArgumentException("Object list is no instance of '".UserList::class."', instance of '".get_class($objectList)."' given.");
-		}
-		
-		if (isset($conditionData['greaterThan'])) {
-			$objectList->getConditionBuilder()->add("YEAR(FROM_UNIXTIME(registrationDate)) <= ?", [date('Y') - ($conditionData['greaterThan'] + 1)]);
-		}
-		
-		if (isset($conditionData['lessThan'])) {
-			$objectList->getConditionBuilder()->add("YEAR(FROM_UNIXTIME(registrationDate)) <= ?", [date('Y') - $conditionData['lessThan']]);
-		}
-	}
+class AnniversaryYearCondition extends AbstractIntegerCondition implements IObjectListCondition
+{
+    /**
+     * @inheritDoc
+     */
+    protected $fieldName = 'registrationDateYear';
+
+    /**
+     * @inheritDoc
+     */
+    protected $label = 'wcf.acp.box.anniversary.condition.year';
+
+    /**
+     * @inheritDoc
+     */
+    protected $description = 'wcf.acp.box.anniversary.condition.year.description';
+
+    /**
+     * @inheritDoc
+     */
+    protected $minValue = 0;
+
+    protected $greaterThan = 0;
+
+    /**
+     * @inheritDoc
+     */
+    protected function getIdentifier()
+    {
+        return 'year';
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getData()
+    {
+        $data = [];
+
+        if ($this->lessThan !== null) {
+            $data['lessThan'] = $this->lessThan;
+        }
+
+        if ($this->greaterThan !== null) {
+            $data['greaterThan'] = $this->greaterThan;
+        } else {
+            $data['greaterThan'] = 0;
+        }
+
+        if (!empty($data)) {
+            return $data;
+        }
+
+        return null;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function addObjectListCondition(DatabaseObjectList $objectList, array $conditionData)
+    {
+        if (!($objectList instanceof UserList)) {
+            throw new InvalidArgumentException("Object list is no instance of '" . UserList::class . "', instance of '" . \get_class($objectList) . "' given.");
+        }
+
+        if (isset($conditionData['greaterThan'])) {
+            $objectList->getConditionBuilder()->add("YEAR(FROM_UNIXTIME(registrationDate)) <= ?", [\date('Y') - ($conditionData['greaterThan'] + 1)]);
+        }
+
+        if (isset($conditionData['lessThan'])) {
+            $objectList->getConditionBuilder()->add("YEAR(FROM_UNIXTIME(registrationDate)) <= ?", [\date('Y') - $conditionData['lessThan']]);
+        }
+    }
 }
